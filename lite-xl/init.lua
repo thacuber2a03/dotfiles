eb28d3df883dc9cff9040180c013ae34405f21fd
thacuber2a03 <antonidiaz200731@gmail.com>
2023-08-26 19:51:44 -0400

    update user module

--- lite-xl/init.lua
+++ lite-xl/init.lua
@@ -1,11 +1,69 @@
-do -- config
-	local config = require 'core.config'
-	config.ignore_files = {}
-	config.indent_size = 2
-	config.tab_type = "hard"
-end
+local common = require 'core.common'
+local config = require 'core.config'
+local style = require 'core.style'
 
-do
+config.ignore_files = {}
+config.indent_size = 2
+config.tab_type = "hard"
+
+config.plugins.tetris.cell_size = 12
+
+style.margin.tab.top = 3
+
+---[[
+do -- wal.lxl
+	local wal = config.plugins.wal
+	if wal.syntax_colors then
+		-- evergreen shit (this took too much)
+		style.syntax.error = { common.color "#ff0000" }
+		wal.syntax_colors["field"] = 1
+		wal.syntax_brightness["field"] = 1.1
+		wal.syntax_colors["punctuation.brackets"] = 3
+		wal.syntax_colors["punctuation.delimiter"] = 3
+		wal.syntax_colors["variable"] = 6
+		wal.syntax_colors["boolean"] = 2
+		wal.syntax_colors["constant"] = 4
+		wal.syntax_colors["float"] = 4
+		wal.syntax_colors["label"] = 2
+		wal.syntax_colors["method.call"] = 5
+		wal.syntax_colors["function.call"] = 1
+		wal.syntax_brightness["function.call"] = 1.8
+		wal.syntax_colors["function.macro"] = 3
+		wal.syntax_brightness["function.macro"] = 1
+		wal.syntax_colors["method"] = 5
+		wal.syntax_colors["type"] = 1
+		wal.syntax_colors["type.builtin"] = 1
+		wal.syntax_brightness["type.builtin"] = 1.5
+		wal.syntax_colors["variable.builtin"] = 4
+		wal.syntax_colors["type.definition"] = 2
+		wal.syntax_colors["definition"] = 2
+		wal.syntax_colors["conditional.ternary"] = 1
+		wal.syntax_colors["keyword.operator"] = 1
+		wal.syntax_brightness["keyword.operator"] = 1.5
+		wal.syntax_colors["punctuation.special"] = 2
+		wal.syntax_colors["storageclass"] = 2
+		wal.syntax_colors["constant.builtin"] = 4
+		wal.syntax_colors["attribute"] = "cursor"
+		wal.syntax_colors["conditional"] = 1
+		wal.syntax_colors["define"] = 4
+		wal.syntax_colors["exception"] = 2
+		wal.syntax_colors["include"] = 4
+		wal.syntax_colors["keyword.function"] = 3
+		wal.syntax_colors["keyword.return"] = 1
+		wal.syntax_brightness["keyword.return"] = 1.5
+		wal.syntax_colors["namespace"] = 1
+		wal.syntax_colors["preproc"] = 2
+		wal.syntax_colors["repeat"] = 1
+		wal.syntax_colors["type.qualifier"] = 1
+		wal.syntax_colors["storageclass.lifetime"] = 2
+
+		local command = require 'core.command'
+		command.perform "wal:apply"
+	end
+end
+--]]
+
+do -- treeview
 	local treeview = require 'plugins.treeview'
 	treeview.visible = false
 end
