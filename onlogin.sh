27894921f519df706774b15855f05ff72355dd39
thacuber2a03 <antonidiaz200731@gmail.com>
2023-08-26 19:52:14 -0400

    update onlogin

--- onlogin.sh
+++ onlogin.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 alsactl -f ~/.config/asound.state restore
 system76-power profile battery
+killall io.elementary.appcenter
+# xrandr --output eDP-1 --scale "1.25x1.25" --filter bilinear
+wal -Rq
