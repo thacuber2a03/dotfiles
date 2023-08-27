387ce9b619807e6fb14bae5972461924bd70dfae
thacuber2a03 <antonidiaz200731@gmail.com>
2023-08-26 20:15:13 -0400

    `md` command

--- fish/config.fish
+++ fish/config.fish
@@ -10,8 +10,21 @@ set -g theme_powerline_fonts yes
 # set -g theme_color_scheme terminal1-dark
 set -g theme_date_format "+%H:%M:%S"
 
-alias l="ls -aF"
+alias ls="exa --color=auto"
+alias l="ls -F"
+alias ll="ls -l "
 alias export="set -gx"
+alias mkdir="mkdir -p"
+alias tree="exa --tree"
+
+function md -d "display a markdown file in Lynx"
+	if test -z $argv[1]
+		printf "usage: md <filename>"
+		return
+	end
+
+	pandoc -fmarkdown -thtml $argv[1] && lynx --stdin
+end
 
 # luarocks
 export LUA_PATH "/usr/local/share/lua/5.4/?.lua;/usr/local/share/lua/5.4/?/init.lua;/usr/local/lib/lua/5.4/?.lua;/usr/local/lib/lua/5.4/?/init.lua;./?.lua;./?/init.lua;/home/thacuber2a03/.luarocks/share/lua/5.4/?.lua;/home/thacuber2a03/.luarocks/share/lua/5.4/?/init.lua"
@@ -26,10 +39,6 @@ export PATH "$HOME/.cargo/bin:$PATH" # cargo and other rust executables
 export PATH "$HOME/.gems/bin:$PATH" # ruby executables
 export PATH "$HOME/.local/bin/node.js/bin:$PATH" # node.js (I will personally never use this myself)
 
-export PATH "$HOME/.local/bin/furnace:$PATH" # furnace
-
-export PATH "$HOME/.local/bin/cool-retro-term:$PATH" # cool retro term
-
 # other vars
 export EDITOR "$HOME/.local/bin/nvim"
 export SHELL (which fish)
@@ -38,15 +47,21 @@ export ODIN_ROOT "$HOME/.local/bin/Odin"
 
 export PLAYDATE_SDK_PATH "$HOME/.local/bin/Playdate SDK"
 
+export BUCKLESPRING_WAV_DIR "$HOME/.local/bin/bucklespring/wav"
+
 #if status is-interactive
 	# Commands to run in interactive sessions can go here
 #end
 
 # uxn roms
-set uxnromsloc "$HOME/roms"
+set uxnromsloc "$HOME/.local/bin/uxn"
 alias donsol="uxnemu -3x $uxnromsloc/donsol.rom"
 alias left="uxnemu -2x $uxnromsloc/left.rom"
 alias bicycle="uxnemu -2x $uxnromsloc/bicycle.rom"
+alias oquonie="uxnemu -2x $uxnromsloc/oquonie.rom"
+alias nasu="uxnemu -2x $uxnromsloc/nasu.rom"
+alias orcaide="uxnemu $uxnromsloc/orca.rom"
+alias uxnlauncher="uxnemu $uxnromsloc/launcher.rom"
 
 function fish_user_key_bindings
 	bind \e\[3\;5~ kill-word
