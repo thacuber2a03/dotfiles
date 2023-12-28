f577e7cd3d51ea1e3a190e4043533e167a3a0c1a
thacuber2a03 <antonidiaz200731@gmail.com>
2023-12-27 23:09:15 -0400

    reorganize files

--- onlogin.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-alsactl -f ~/.config/asound.state restore
-system76-power profile battery
-killall io.elementary.appcenter
-# xrandr --output eDP-1 --scale "1.25x1.25" --filter bilinear
-wal -Rq
